07ace7067cda8015fd39a0e9d853fd58653b33c7
nadia_lovely_29rpl <80080969+nadialvy@users.noreply.github.com>
2023-02-06 12:23:43 +0700

    menu card hovered

--- src/components/MenuCard.vue
+++ src/components/MenuCard.vue
@@ -3,10 +3,10 @@
         <div class="w-32 h-32 rounded-full overflow-hidden shadow-xl" style="z-index: 1;">
             <img :src="img" class="w-full h-full object-cover" />
         </div>
-        <div class="w-48 h-56 px-4 relative bottom-20 bg-white rounded-2xl shadow-lg">
-            <h2 class="pt-24 font-bold text-blue-300 text-start mt-4 truncate" style="width: 10rem; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ name }}</h2>
-            <img src="../assets/star.svg" class="mb-4" />
-            <p class="font-semibold text-orange-500 mt-2">Rp.{{ price }}</p>
+        <div class="w-48 h-56 px-4 relative bottom-20 bg-white hoverable hover:bg-orange-400 rounded-2xl shadow-lg">
+            <h2 class="pt-24 font-bold text-blue-300 text-start mt-4 truncate hover-white">{{ name }}</h2>
+            <img src="../assets/star.svg" class="mb-4 img-change" />
+            <p class="font-semibold text-orange-500 mt-2 hover-white">Rp.{{ price }}</p>
         </div>
     </div>
 </template>
@@ -18,4 +18,13 @@
         price: String,
     })
 </script>
+
+<style>
+  .hoverable:hover .hover-white {
+    color: white;
+  }
+  .hoverable:hover .img-change {
+    content: url("../assets/star-hover.svg");
+  }
+</style>
 
