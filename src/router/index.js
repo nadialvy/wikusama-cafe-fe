7544a7199bf12959a6926f540a6db284d2bae570
nadia_lovely_29rpl <80080969+nadialvy@users.noreply.github.com>
2023-02-06 07:42:25 +0700

    protect route

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Table from "../views/Table.vue";
 import Order from "../views/Order.vue";
 import User from "../views/User.vue";
 import Login from "../views/Login.vue";
+import store from "../store/index.js"
 
 const routes = [
     {
@@ -18,31 +19,43 @@ const routes = [
         path: "/menu",
         name: "menu",
         component: Menu,
+        meta: { requiresAuth: true }
     },
     {
         path: "/menu/:id",
         name: "detailmenu",
         component: DetailMenu,
+        meta: { requiresAuth: true }
     },
     {
         path: "/table",
         name: "table",
         component: Table,
+        meta: { requiresAuth: true }
     },
     {
         path: "/user",
         name: "user",
         component: User,
+        meta: { requiresAuth: true }
     },
     {
         path: "/order",
         name: "order",
         component: Order,
+        meta: { requiresAuth: true }
     },
     {
-        path: "/login",
-        name: "login",
+        path: '/login',
+        name: 'login',
         component: Login,
+        beforeEnter: (to, from, next) => {
+            if(store.state.auth.status.loggedIn){
+                next({ name: 'home' })
+            } else {
+                next();
+            }
+        }
     },
 ]
 
@@ -51,5 +64,20 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+router.beforeEach((to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresAuth)){
+        if(!store.state.auth.status.loggedIn){
+            next({
+                path: '/login',
+                query: { redirect: to.fullPath }
+            })
+        }else{
+            next()
+        }
+    }else{
+        next()
+    }
+});
 
 export default router
